3b2b26831cce1b719934f52049d174a3e5050386
Pomalini <42066228+Pomalini@users.noreply.github.com>
2018-12-19 18:33:00 +0530

    Update VowelOrCons.java

--- VowelOrCons.java
+++ VowelOrCons.java
@@ -4,11 +4,15 @@ public class VowelOrCons {
 	public static void main(String args[]) {
 	Scanner scan=new Scanner(System.in);
     char ch=scan.next().charAt(0);
+    ch=Character.toLowerCase(ch);
     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
     	System.out.println("Vowel");
     }
-    else {
+    else if(ch>='a'&&ch<='z') {
     	System.out.println("Consonant");
+    }
+    else {
+    	System.out.println("Invalid");
     }
     
 
