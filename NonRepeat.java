1201c428aacecb91e89775b96ca55109fa3c2850
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-02-07 21:45:47 +0530

    Create NonRepeat.java

---
+++ NonRepeat.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class NonRepeat {
+	public static void main (String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=scan.nextInt();
+		    
+		}
+		int count=0;
+		for(int i=0;i<n;i++){
+		    count=0;
+		    for(int j=0;j<n;j++){
+		        if(arr[i]==arr[j]){
+		            count++;
+		        }
+		    }
+
+		    if(count==1){
+		        System.out.println(arr[i]);
+		    }
+		}
+	}
+}
