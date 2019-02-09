a65a03fae44caf7ad95ae5cbca9d96d07dd1fc9a
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-02-09 11:27:38 +0530

    Create FullStop.java

---
+++ FullStop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class FullStop {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        String str=scan.next();
+        System.out.println(str+".");
+        
+    }
+    
+}
