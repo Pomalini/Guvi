7e5e626d11f2fa866d6ba029b1054bd157658480
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-04-23 12:10:02 +0530

    Create PowerOf.java

---
+++ PowerOf.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math; 
+
+
+public class PowerOf {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int k=scan.nextInt();
+		System.out.println((int)Math.pow(n,k));
+	}
+
+}
