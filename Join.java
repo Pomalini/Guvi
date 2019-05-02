e157ac93585b70e8e0ab825f50ba16971e39b372
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-05-02 21:47:21 +0530

    Create Join.java

---
+++ Join.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Join {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		String str1=scan.next();
+		String str2=scan.next();
+		System.out.println(str1+str2);
+		
+	}
+
+}
